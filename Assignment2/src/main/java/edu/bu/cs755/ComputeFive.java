2ef09322b957493db51a625a5bab84f591feaba6
Don Yoo <dyoo@semigear.com>
2018-02-26 10:35:05 -0500

    task2 job2

---
+++ Assignment2/src/main/java/edu/bu/cs755/ComputeFive.java
@@ -0,0 +1,19 @@
+package edu.bu.cs755;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class ComputeFive extends Reducer<DoubleWritable, Text , Text, DoubleWritable> {
+    
+    public void reduce(DoubleWritable values, Iterable<Text> keyIn, Context context) throws IOException, InterruptedException {
+    	
+    	//System.out.println("\n reduce ");
+    	for (Text key : keyIn) {
+    		//System.out.println("key " + key + " value "+ values);
+            context.write(key, values);
+        }
+    }
+}
